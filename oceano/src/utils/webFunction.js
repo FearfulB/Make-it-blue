49520bee5fa7f37d91f5c7aaf165aab68035b50a
johann <jbois@gaming.tech>
2024-02-08 20:39:35 +0100

    frontend

--- oceano/src/utils/webFunction.js
+++
@@ -1,4 +0,0 @@
-export function isSelected(div, css) {
-    var selected = document.getElementById(div)
-    selected.classList.contains(css) ? selected.classList.remove(css) : selected.classList.add(css)
-}
